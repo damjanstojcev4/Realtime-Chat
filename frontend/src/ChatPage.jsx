5be61b2d5a5885095d20d9460cea6559166afd3b
Damjan Stojcev <damjan.stojcev4@gmail.com>
2023-09-26 12:32:23 +0200

    Finished App

--- frontend/src/ChatPage.jsx
+++ frontend/src/ChatPage.jsx
@@ -1,5 +1,14 @@
-const ChatsPage = () => {
-    return <>chats...</>
+import {PrettyChatWindow} from 'react-chat-engine-pretty'
+
+const ChatsPage = (props) => {
+    return (
+    <div style={{height: '100vh'}}>
+        <PrettyChatWindow
+            projectId='2dd87f95-bac5-49d1-a0b8-999c93bceae2'
+            username={props.user.username}
+            secret={props.user.secret}
+            style={{height: '100%'}} />
+    </div>)
 }
 
 export default ChatsPage;
